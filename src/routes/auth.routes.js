1384b0b787b3f59c208712084378a5c6e3620d2c
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2026-03-25 22:53:42 +0530

    [ShiftSpace]: logout user

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,16 @@
 import { Router } from "express";
-import { loginUser, registerUser } from "../controllers/auth.controllers.js";
+import { loginUser, logoutUser, registerUser } from "../controllers/auth.controllers.js";
 import { validate } from "../middlewares/validator.middleware.js";
 import { userLoginValidator, userRegisterValidator } from "../validators/index.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+// unsecured route
 router.route("/register").post(userRegisterValidator(), validate, registerUser);
 router.route("/login").post(userLoginValidator(), validate, loginUser);
+
+//secure routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
