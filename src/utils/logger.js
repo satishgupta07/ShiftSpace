fabcfb2e7d716602dfa1510b6ee5a90d02291272
Satish Kumar Gupta <satishgupta.cse01@gmail.com>
2026-04-10 03:48:54 +0530

    [ShiftSpace]: updated logs format

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,18 +1,21 @@
 import { createLogger, format, transports } from "winston";
-const { combine, timestamp, json, colorize } = format;
+const { combine, timestamp, json } = format;
+
+const timestampFormat = timestamp({ format: "ddd, MMMM Do YYYY, h:mm:ss a" })
 
 // Custom format for console logging with colors
 const consoleLogFormat = format.combine(
     format.colorize(),
+    timestampFormat,
     format.printf(({ level, message, timestamp }) => {
-        return `${level}: ${message}`;
+        return `${timestamp} - ${level}: ${message}`;
     })
 );
 
 // Create a Winston logger
 const logger = createLogger({
     level: "info",
-    format: combine(colorize(), timestamp(), json()),
+    format: combine(timestampFormat, json()),
     transports: [
         new transports.Console({
             format: consoleLogFormat,
